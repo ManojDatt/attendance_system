807e505b84a502deead0c25079ab62ab5c0c75ea
manojdatt <manojdatt1it@gmail.com>
2017-04-04 10:42:12 +0530

    side bar with logout and latin layout added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do  
   devise_for :admins, skip: :registrations
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :attendances, only:[:index,:create]
+  resources :attendances, only:[:index]
   devise_for :developers, skip: :registrations
   root "attendances#index"
   get "punch_out"=>"attendances#punch_out", as: :punch_out
