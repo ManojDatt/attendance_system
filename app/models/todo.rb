77e82c72bbc3805c1251bbfe138e3c6a148627fe
Manish Kumar Gupta <manish.gupta@arkasoftwares.com>
2017-04-04 13:16:45 +0530

    added todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,6 @@ class Todo < ApplicationRecord
   belongs_to :project
   belongs_to :developer
   # enum task_status:["Pending","Complete"]
+   enum task_type:["Task","Research"]
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do  
-  devise_for :admins, skip: :registrations
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :projects
-  resources :todos,only:[:new,:create]
-  resources :attendances, only:[:index]
-  devise_for :developers, skip: :registrations
-  root "attendances#index"
-  get "punch_out"=>"attendances#punch_out", as: :punch_out
-  get 'developer-profile'=> "attendances#get_developer_profile" , as: :get_developer_profile
+	devise_for :admins, skip: :registrations
+	mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+	resources :projects
+	resources :todos,only:[:new,:create] do
+		collection do
+			get 'today_research'
+			post 'submit_research'
+		end
+	end
+	resources :attendances, only:[:index]
+	devise_for :developers, skip: :registrations
+	root "attendances#index"
+	get "punch_out"=>"attendances#punch_out", as: :punch_out
+	get 'developer-profile'=> "attendances#get_developer_profile" , as: :get_developer_profile
 end
